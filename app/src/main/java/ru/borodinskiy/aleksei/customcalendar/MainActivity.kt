5d86100bd201fa1c2644afb5a85c960dd5b3b70e
Borodinskiy88 <a-borodinskiy@mail.ru>
2023-11-20 11:05:34 +0300

    text

--- app/src/main/java/ru/borodinskiy/aleksei/customcalendar/MainActivity.kt
+++ app/src/main/java/ru/borodinskiy/aleksei/customcalendar/MainActivity.kt
@@ -1,10 +1,12 @@
 package ru.borodinskiy.aleksei.customcalendar
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,17 +17,27 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -69,7 +81,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//var isChooseDate = false
 @Composable
 fun MainScreen(adjacentMonths: Long = 500) {
     val today = remember { LocalDate.now() }
@@ -80,7 +91,7 @@ fun MainScreen(adjacentMonths: Long = 500) {
     val daysOfWeek = remember { daysOfWeek() }
 
     //Visible
-    var isChooseDate = true
+    var isChooseDate by remember { mutableStateOf(true) }
 
     Surface(
         color = Color.Black
@@ -105,7 +116,7 @@ fun MainScreen(adjacentMonths: Long = 500) {
             Column(
                 modifier = Modifier
                     //todo сделать верх уже низа
-//                    .padding(horizontal = 20.dp)
+//                    .padding(20.dp)
                     .clip(RoundedCornerShape(20.dp, 20.dp, 20.dp, 20.dp))
                     .background(Color.Green)
             ) {
@@ -306,15 +317,83 @@ fun MainScreen(adjacentMonths: Long = 500) {
                             .fillMaxWidth()
                             .fillMaxHeight()
                             .padding(top = 16.dp)
-                            .background(color = Color.LightGray)
+                            .background(color = Color.LightGray),
+                        verticalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Text(text = "Запросить на смену")
-
+                        Column {
+                            Text(text = "Запросить на смену")
+                            Menu()
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            verticalAlignment = Alignment.Bottom
+                        ) {
+                            Button(
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .padding(bottom = 16.dp)
+                                    .fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.Black,
+                                    contentColor = Color.White
+                                )
+                            ) {
+                                Text(
+                                    text = "Сохранить",
+                                )
+                            }
+                        }
 
                     }
                 }
             }
 
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Menu() {
+    val options = listOf("Выходной", "Больничный")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
+// We want to react on tap/press on TextField to show menu
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier
+                .padding(top = 4.dp)
+        ) {
+            TextField(
+                // The `menuAnchor` modifier must be passed to the text field for correctness.
+                modifier = Modifier.menuAnchor(),
+                readOnly = true,
+                value = selectedOptionText,
+                onValueChange = {},
+                label = null,
+                //                          label = { Text("Label") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                options.forEach { selectionOption ->
+                    DropdownMenuItem(
+                        text = { Text(selectionOption) },
+                        onClick = {
+                            selectedOptionText = selectionOption
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
         }
     }
 }
